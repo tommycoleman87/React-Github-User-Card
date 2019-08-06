c329e91d2bf3ffd6f1bab80aabd6f7acb2780dac
Tommy Coleman <tommiilee@gmail.com>
2019-08-06 19:40:08 -0400

    changed the color of the heart

--- github-cards/src/components/PageHeader.js
+++ github-cards/src/components/PageHeader.js
@@ -5,7 +5,7 @@ import { Header, Image } from 'semantic-ui-react'
 const PageHeader = () => {
     return <Header as='h1' style={{display: 'flex', justifyContent: 'space-around', width: '50%', margin: 'auto'}}>
         <Image src={lambdalogo} />
-        ♥♥️♥️♥️'s
+        ❤️'s
        <Image src={githublogo} />
     </Header>
 }
